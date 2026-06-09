c7a140d9d2a9dd789e5c2ee6d355ca0e0422db30
Gabriel Gomes <gabrielgfernandes1977@gmail.com>
2026-06-09 20:04:39 -0300

    renomeando pastas para melhor organização

--- algoritmo/1semestre/atividades/atividade 5/app/src/routes/senas.routes.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require("express");
-const { getConcurso, last } = require("../repositories/senas.repository");
-const routes = Router();
-routes.get("/", last);
-routes.get("/:concurso", getConcurso);
-module.exports = routes;
