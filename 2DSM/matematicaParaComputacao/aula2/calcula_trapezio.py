b909985a33072027308b1d6cd080351075c6c85b
Gabriel Gomes <gabrielgfernandes1977@gmail.com>
2026-08-13 22:00:44 -0300

    Adicionando aula 2 de matematica para computação

---
+++ 2DSM/matematicaParaComputacao/aula2/calcula_trapezio.py
@@ -0,0 +1,3 @@
+def calcula_trapezio(base_menor, base_maior, altura):
+    resultado = (base_menor + base_maior) * altura / 2 
+    return print(resultado)
---
+++ 2DSM/matematicaParaComputacao/aula2/importa_funcao.py
@@ -0,0 +1,7 @@
+import calcula_trapezio as calculo 
+
+base_menor = float(input("Insira a base menor:"))
+base_maior = float(input("Insira a base maior:"))
+altura = float(input("Insira a altura:"))
+
+resultado = calculo.calcula_trapezio(base_menor, base_maior, altura)
